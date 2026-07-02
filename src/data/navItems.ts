9803e4b70bc044dee046f2db486c199c2a16977a
brownng2 <brownng2@vcu.edu>
2026-07-02 17:15:03 +0000

    created navItems

---
+++ src/data/navItems.ts
@@ -0,0 +1,11 @@
+export const navItems = [
+    { name: "Dashboard", href: "/", emoji: "e"},
+    { name: "Books", href: "/books", emoji: "e"},
+    { name: "Fitness", href: "/fitness", emoji: "e"},
+    { name: "Bible", href: "/bible", emoji: "e"},
+    { name: "To-Dos", href: "/todos", emoji: "e"},
+    { name: "Calendar", href: "/calendar", emoji: "e"},
+    { name: "Watchlist", href: "/watchlist", emoji: "e"},
+    { name: "Music", href: "/music", emoji: "e"},
+]
+
